2109defb4c0d8ad5f74609f27364ef1ebf129c5f
MiKhai37 <mi.khai.thai@gmail.com>
2021-11-02 15:03:06 +0100

    add cors origin

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors({
-  origin: ['http://localhost:3000/', 'https://scrabble-clone-front.vercel.app/']
+  origin: ['http://localhost:3000', 'https://scrabble-clone-front.vercel.app']
 }));
 
 app.use(logger('dev'));
